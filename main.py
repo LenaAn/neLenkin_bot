e540b232144af017542777a3c385aed3d04a0398
Elena Anyusheva <baajina@gmail.com>
2025-03-26 15:44:36 +0100

    add description on how to join

--- main.py
+++ main.py
@@ -11,34 +11,31 @@ logging.basicConfig(
 )
 
 
-def build_menu(
-        buttons: List[InlineKeyboardButton],
-        n_cols: int,
-        header_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]] = None,
-        footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]] = None
-) -> List[List[InlineKeyboardButton]]:
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons if isinstance(header_buttons, list) else [header_buttons])
-    if footer_buttons:
-        menu.append(footer_buttons if isinstance(footer_buttons, list) else [footer_buttons])
-    return menu
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def main_menu() -> InlineKeyboardMarkup:
     button_list = [
-        InlineKeyboardButton("Как вступить?", callback_data="how_to_add"),
+        InlineKeyboardButton("Как вступить?", callback_data="how_to_join"),
         InlineKeyboardButton("Хочу читать Кабанчика!", callback_data="ddia"),
         InlineKeyboardButton("Хочу читать SRE Book!", callback_data="sre_book")
     ]
-    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
+    menu = [button_list[i:i + 1] for i in range(0, len(button_list), 1)]
+    return InlineKeyboardMarkup(menu)
 
+
+def back_menu() -> InlineKeyboardMarkup:
+    button_list = [
+        InlineKeyboardButton("Назад", callback_data="back"),
+    ]
+    menu = [button_list[i:i + 1] for i in range(0, len(button_list), 1)]
+    return InlineKeyboardMarkup(menu)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     club_description = \
         ("(не)Ленкин клуб @lenka_ne_club — это клуб для тех, кто хочет становиться лучше как системный "
          "(инфраструктурный) программист (в противовес продуктовой разработке). \n\n"
          "Обсуждаем распределенные системы, базы данных, data streaming и все, что рядом находится. Фокус — прикладные "
          "знания, которые помогут в работе.")
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=club_description, reply_markup=reply_markup)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=club_description, reply_markup=main_menu())
 
 
 async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -46,16 +43,27 @@ async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()  # Acknowledge the callback
 
-    if query.data == "col1_action":
-        response_text = "You clicked Column 1!"
-    elif query.data == "col2_action":
-        response_text = "You clicked Column 2!"
+    if query.data == "back":
+        await start(update, context)
+        return
+
+    if query.data == "how_to_join":
+        response_text = ("Просто вступить в группу и написать интро про себя с хэштегом #whois — чем занимаешься, где "
+                         "работаешь или учишься, где живешь, в какой активности хочешь участвовать в клубе.\n\n"
+                         "Клуб держится на доверии и активном участии членов клуба. Когда ты пишешь содержательное "
+                         "интро о себе, ты даешь возможность другим членам клуба задать тебе вопрос / попросить совета "
+                         "/ позвать на сходку.\n\n"
+                         "Интро уже состоящих в клубе людей ты можешь найти по хэштегу #whois. \n\n"
+                         "Вступить ➡️ @lenka_ne_club")
     elif query.data == "row2_action":
         response_text = "You clicked Row 2!"
     else:
         response_text = "Unknown action."
 
-    await context.bot.send_message(response_text)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=response_text,
+        reply_markup=back_menu())
 
 
 async def command_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
