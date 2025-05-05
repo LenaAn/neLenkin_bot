452323529ff79dec1b79a546b1d1aec0eedef355
Elena Anyusheva <baajina@gmail.com>
2025-05-05 18:21:43 +0200

    fix unused settings

--- settings.py
+++ settings.py
@@ -6,6 +6,3 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_BOT_WEBHOOK_URL = os.getenv('TELEGRAM_BOT_WEBHOOK_URL')
-TELEGRAM_BOT_WEBHOOK_HOST = "0.0.0.0"
-TELEGRAM_BOT_WEBHOOK_PORT = 8816
