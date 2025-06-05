b5bb47804bdf16bd37700b7e8faf4e913841a15a
Elena Anyusheva <baajina@gmail.com>
2025-06-05 16:50:20 +0200

    add handlers to handle leetcode subscriptions

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,9 +1,14 @@
 import logging
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+
+from sqlalchemy import exists, select
+from sqlalchemy.orm import Session
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, User
 from telegram.ext import ContextTypes
 
 import constants
 import helpers
+import models
 import settings
 
 
@@ -76,23 +81,47 @@ async def handle_sre_book(update: Update) -> None:
         parse_mode="HTML")
 
 
+def user_is_enrolled_in_leetcode(tg_user: User) -> bool:
+    with Session(models.engine) as session:
+        users_exists = session.scalar(
+            select(exists().where((models.Enrollment.tg_id == str(tg_user.id)) & (models.Enrollment.course_id == 7)))
+        )
+    if users_exists:
+        logging.info(f"user is already enrolled in Leetcode Mocks: {tg_user}")
+    else:
+        logging.info(f"user is not already enrolled in Leetcode Mocks: {tg_user}")
+    return users_exists
+
+
 async def handle_mock_leetcode(update: Update) -> None:
-    logging.info(f"mock_leetcode triggered by {helpers.get_user(update)}")
-    # todo:
-    # if user is enrolled, show "Перестать получать уведомления", otherwise show "Хочу получать уведомления!"
-    button_list = [
-        InlineKeyboardButton("Хочу участвовать!", callback_data="leetcode_enroll"),
-        InlineKeyboardButton("Назад", callback_data="back"),
-    ]
-    menu = [button_list[i:i + 1] for i in range(0, len(button_list), 1)]
-    await update.callback_query.edit_message_text(
-        text=constants.mock_leetcode_description,
-        reply_markup=InlineKeyboardMarkup(menu),
-        parse_mode="HTML")
+    tg_user = helpers.get_user(update)
+    logging.info(f"mock_leetcode triggered by {tg_user}")
+
+    if user_is_enrolled_in_leetcode(tg_user):
+        button_list = [
+            InlineKeyboardButton("Перестать получать уведомления", callback_data="leetcode_unenroll"),
+            InlineKeyboardButton("Назад", callback_data="back"),
+        ]
+        menu = [button_list[i:i + 1] for i in range(0, len(button_list), 1)]
+        await update.callback_query.edit_message_text(
+            text=constants.mock_leetcode_description + "\n\n" + constants.leetcode_enroll_description,
+            reply_markup=InlineKeyboardMarkup(menu),
+            parse_mode="HTML")
+    else:
+        button_list = [
+            InlineKeyboardButton("Хочу участвовать!", callback_data="leetcode_enroll"),
+            InlineKeyboardButton("Назад", callback_data="back"),
+        ]
+        menu = [button_list[i:i + 1] for i in range(0, len(button_list), 1)]
+        await update.callback_query.edit_message_text(
+            text=constants.mock_leetcode_description + "\n\n" + constants.leetcode_cta_description,
+            reply_markup=InlineKeyboardMarkup(menu),
+            parse_mode="HTML")
 
 
 async def handle_leetcode_enroll(update: Update) -> None:
     logging.info(f"leetcode_enroll handled by {helpers.get_user(update)}")
+    # todo: actually store user in enrollments table
     button_list = [
         InlineKeyboardButton("Я передумала, отписаться", callback_data="leetcode_unenroll"),
         InlineKeyboardButton("Назад", callback_data="back"),
@@ -107,7 +136,16 @@ async def handle_leetcode_enroll(update: Update) -> None:
 
 async def handle_leetcode_unenroll(update: Update) -> None:
     logging.info(f"leetcode_unenroll handled by {helpers.get_user(update)}")
-    await handle_mock_leetcode(update)
+    # todo: actually delete user from enrollments table
+    button_list = [
+        InlineKeyboardButton("Назад", callback_data="back"),
+    ]
+    menu = [button_list[i:i + 1] for i in range(0, len(button_list), 1)]
+    await update.callback_query.edit_message_text(
+        text=constants.leetcode_unenroll_description,
+        reply_markup=InlineKeyboardMarkup(menu),
+        parse_mode="HTML"
+    )
 
 
 async def handle_how_to_present(update: Update) -> None:
