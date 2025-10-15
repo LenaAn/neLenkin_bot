ba2745211a594e1c1aebb9746aecf7af757787d2
Elena Anyusheva <baajina@gmail.com>
2025-10-15 20:23:23 +0200

    store patrons linking in handler

--- handlers/patreon_handlers.py
+++ handlers/patreon_handlers.py
@@ -1,9 +1,13 @@
 import logging
 
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import Session
 from telegram import Update
 from telegram.ext import (CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters)
 
 import helpers
+import models
+import settings
 from patreon import fetch_patrons
 
 CONNECT_PATREON = 1
@@ -18,6 +22,43 @@ async def start_connect_patreon(update: Update, context: ContextTypes.DEFAULT_TY
     return CONNECT_PATREON
 
 
+# we will store it regardless it's a paying patron or not
+# we just store the pairing of tg_id to patreon email
+# to know if it's paying patron or not, check redis
+async def store_patreon_linking(update: Update, patron_email: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    with Session(models.engine) as session:
+        user = helpers.get_user(update)
+        patreon_link = {
+            'tg_id': user.id,
+            'tg_username': user.username,
+            'patreon_email': patron_email
+        }
+        stmt = insert(models.PatreonLink).values(**patreon_link)
+        stmt = stmt.on_conflict_do_update(
+            constraint='PatreonLink_pkey',
+            set_=patreon_link
+        )
+        session.execute(stmt)
+        try:
+            session.commit()
+            logging.info(f"Added new patron linking: {user.username} to {patron_email} to db")
+        except Exception as e:
+            # I don't rely on handlers.handlers.error_handler because in this case ConversationHandler.END will not be
+            # returned and commands after this exception will be ignored
+            session.rollback()
+            logging.warning(f"Didn't add patron linking: {user.username} to {patron_email} to db: {e}")
+            await context.bot.send_message(
+                chat_id=settings.ADMIN_CHAT_ID,
+                text=f"Не смог прявязать Patreon почту {patron_email} к пользователю {user.username}: {e}"
+            )
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"Упс! Случилась ошибка, но проблема не в тебе! Уже оповестил @lenka_colenka"
+            )
+            return False
+    return True
+
+
 async def connect_with_email(update: Update, context: ContextTypes.DEFAULT_TYPE) \
         -> int:
     logging.info(f"connect_with_email handler triggered by {helpers.repr_user_from_update(update)}")
@@ -27,12 +68,15 @@ async def connect_with_email(update: Update, context: ContextTypes.DEFAULT_TYPE)
     # this method loads patrons from patreon and does lookup in redis
     patron_info = fetch_patrons.get_patron_by_email(email_to_find)
     if patron_info:
-        # todo: analyze if this is a paying and active patron
-        logging.info(f"Patron found for email {email_to_find}: {patron_info}")
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Нашла патрона! {patron_info}",
-        )
+        if await store_patreon_linking(update, email_to_find, context):
+            # todo: analyze if this is a paying and active patron
+            logging.info(f"Patron found for email {email_to_find}: {patron_info}")
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"Нашла патрона! {patron_info}",
+            )
+        else:
+            return ConversationHandler.END
     else:
         logging.info(f"Could not find patron for email {email_to_find}")
         await context.bot.send_message(
