ec2032ca11c5a46e0953a8f02349365e46dc7ece
Elena Anyusheva <baajina@gmail.com>
2025-11-06 20:01:08 +0100

    fix typo

--- handlers/patreon_handlers.py
+++ handlers/patreon_handlers.py
@@ -74,7 +74,7 @@ async def connect_with_email(update: Update, context: ContextTypes.DEFAULT_TYPE)
     if patron_info:
         if await store_patreon_linking(update, email_to_find, context):
             logging.info(f"Patron found for email {email_to_find}: {patron_info}")
-            msg: str = f"Нашла твой профиль Patron: {email_to_find}.\n\n"
+            msg: str = f"Нашла твой профиль Patreon: {email_to_find}.\n\n"
             # todo: call reply_for_patreon_members or reply_for_basic_with_linked_patreon here
             donate_amount_cents = int(patron_info['currently_entitled_amount_cents'])
             if donate_amount_cents >= 1500:
