0af5858c7e7f4ae4469436610044c346937635d3
Elena Anyusheva <baajina@gmail.com>
2025-11-28 14:50:14 +0100

    add a Leetcode Mocks topic to .env

--- settings.py
+++ settings.py
@@ -15,3 +15,6 @@ DB_NAME = 'nelenkin_club'
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 ADMIN_CHAT_ID = int(os.getenv('ADMIN_CHAT_ID'))
+
+CLUB_GROUP_CHAT_ID = int(os.getenv('CLUB_GROUP_CHAT_ID'))
+LEETCODE_MOCKS_THREAD_ID = int(os.getenv('LEETCODE_MOCKS_THREAD_ID'))
