cb6b5a0edcb1ac3b7bcf681642c0ef3bfde41ac2
Elena Anyusheva <baajina@gmail.com>
2026-04-16 22:03:07 +0200

    print user id if not tg usrname in leetcode pairs

--- leetcode_pairs/leetcode_notifications.py
+++ leetcode_pairs/leetcode_notifications.py
@@ -10,6 +10,14 @@ import settings
 from zoneinfo import ZoneInfo
 
 
+def print_user(user: models.User):
+    if user.tg_username:
+        return f"@{user.tg_username}"
+
+    safe_name: str = user.first_name if user.first_name else user.tg_id
+    return f'<a href="tg://user?id={user.tg_id}">{safe_name}</a>'
+
+
 async def send_leetcode_pairs_to_group(context: ContextTypes.DEFAULT_TYPE,
                                        generate_graph_obj: generate_graph.GenerateLeetcodeMocks):
     emoji = helpers.random_neutral_emoji()
@@ -17,37 +25,36 @@ async def send_leetcode_pairs_to_group(context: ContextTypes.DEFAULT_TYPE,
     if len(generate_graph_obj.pairs) > 0:
         notification_str += f"Пары на эту неделю:\n\n"
         for pair in generate_graph_obj.pairs:
-            notification_str += f"{emoji} @{pair.first.tg_username} — @{pair.second.tg_username}\n"
+            notification_str += f"{emoji} {print_user(pair.first)} — {print_user(pair.second)}\n"
         notification_str += f"Напиши партнеру и договорись о времени!\n\n"
     else:
         notification_str += "Пар на этой неделе нет 😢\n\n"
 
-    # todo: handle NULL usernames
     if len(generate_graph_obj.without_pairs) > 0:
         notification_str += "Без пары на этой неделе "
-        notification_str += ", ".join([f"@{user.tg_username}" for user in generate_graph_obj.without_pairs])
+        notification_str += ", ".join([f"{print_user(user)}" for user in generate_graph_obj.without_pairs])
         notification_str += ". Можно написать в личку и договориться о моке!\n"
 
     await context.bot.send_message(
         chat_id=settings.CLUB_GROUP_CHAT_ID,
         message_thread_id=settings.LEETCODE_MOCKS_THREAD_ID,
-        text=notification_str
+        text=notification_str,
+        parse_mode="HTML"
     )
 
 
 async def unicast_leetcode_partner(context: ContextTypes.DEFAULT_TYPE,
                                    generate_graph_obj: generate_graph.GenerateLeetcodeMocks):
     logging.info(f"in unicast_leetcode_partner: {generate_graph_obj.pairs=}")
-    # todo: handle NULL usernames
     # todo: add info about chosen timeslots
     for pair in generate_graph_obj.pairs:
         await context.bot.send_message(
             chat_id=pair.first.tg_id,
-            text=f"Твоя пара на Leetcode мок: @{pair.second.tg_username}. Напиши партнеру и договорись о времени!",
+            text=f"Твоя пара на Leetcode мок: {print_user(pair.second)}. Напиши партнеру и договорись о времени!",
             parse_mode="HTML")
         await context.bot.send_message(
             chat_id=pair.second.tg_id,
-            text=f"Твоя пара на Leetcode мок: @{pair.first.tg_username}. Напиши партнеру и договорись о времени!",
+            text=f"Твоя пара на Leetcode мок: {print_user(pair.first)}. Напиши партнеру и договорись о времени!",
             parse_mode="HTML")
 
 
