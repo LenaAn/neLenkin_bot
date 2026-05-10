ef47fb3e2982fa2d1f103ca2609048e107304a0d
Elena Anyusheva <baajina@gmail.com>
2026-05-10 23:18:32 +0200

    report users with any enrollment and users with active enrollments

--- monitoring/calculate_metrics_and_report.py
+++ monitoring/calculate_metrics_and_report.py
@@ -3,7 +3,7 @@ from datetime import date
 import logging
 from monitoring.push_monitoring import metrics
 
-from sqlalchemy import func
+from sqlalchemy import distinct, func
 from sqlalchemy.orm import Session
 from telegram import Bot
 from membership import fetch_boosty_patrons, fetch_patrons, membership
@@ -31,6 +31,24 @@ def users_failed_broadcast_count() -> int:
         failed_users_count = session.query(models.User).filter(models.User.is_last_message_successful.is_(False)).count()
     logger.info(f"users_failed_broadcast_count: {failed_users_count}")
     return failed_users_count
+
+
+def get_users_with_any_enrollment_count() -> int:
+    with Session(models.engine) as session:
+        users_with_any_enrollment_count = session.query(
+            func.count(distinct(models.Enrollment.tg_id))
+        ).scalar() or 0
+    logger.info(f"{users_with_any_enrollment_count=}")
+    return users_with_any_enrollment_count
+
+
+def get_users_with_active_enrollment_count() -> int:
+    with Session(models.engine) as session:
+        users_with_active_enrollment_count = (session.query(func.count(distinct(models.Enrollment.tg_id)))
+                .join(models.Course, models.Course.id == models.Enrollment.course_id)
+                .filter(models.Course.is_active.is_(True)).scalar() or 0)
+    logger.info(f"{users_with_active_enrollment_count=}")
+    return users_with_active_enrollment_count
 
 
 def set_activity_members() -> None:
@@ -152,10 +170,17 @@ async def calculate_metrics_and_report(bot: Bot = None) -> None:
     try:
         metrics.set("users_started_bot", users_started_bot_count())
         metrics.set("users_failed_broadcast", users_failed_broadcast_count())
+
+        # membership
         metrics.set("patreon_patrons", len(fetch_patrons.get_patrons_from_redis("active_patron")))
         metrics.set("boosty_patrons", len(fetch_boosty_patrons.get_boosty_patrons_from_redis(1)))
         set_activity_members()
 
+        # users enrolled to any course
+        metrics.set("users_with_any_enrollment_total", get_users_with_any_enrollment_count())
+        metrics.set("users_with_active_enrollment_total", get_users_with_active_enrollment_count())
+
+        # per-course enrollment
         set_enrolled_users_map()
         set_enrolled_users_paid_map()
         set_enrolled_users_activity_membership_map()
