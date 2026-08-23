a5acdfa89c609c50d6c70ee581fecb8179e860b3
Elena Anyusheva <baajina@gmail.com>
2026-08-24 00:38:13 +0200

    create mock pairs only for signups of this year

--- leetcode_pairs/leetcode_notifications.py
+++ leetcode_pairs/leetcode_notifications.py
@@ -98,7 +98,7 @@ async def leetcode_notifications(context: ContextTypes.DEFAULT_TYPE):
         return
 
     generate_graph_obj = generate_graph.GenerateLeetcodeMocks.build(
-        week_number=datetime.date.today().isocalendar().week)
+        week_number=datetime.date.today().isocalendar().week, year=datetime.datetime.today().isocalendar().year)
 
     await send_leetcode_pairs_to_group(context, generate_graph_obj)
     await unicast_leetcode_partner(context, generate_graph_obj)
